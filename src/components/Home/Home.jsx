871a68513608546e8136813ea7630e148d29c994
adibmahmud007 <adibmahmud307@gmail.com>
2024-03-12 22:54:12 +0600

    Home bg added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import "../../App.css"
 
 const Home = () => {
     return (
         <div className="flex flex-col">
-            <div className="min-h-screen">
+            <div className="min-h-screen home_bg">
             <Header></Header>
             <Outlet></Outlet>
             </div>
---
+++ src/components/HomeBG/HomeBG.jsx
@@ -0,0 +1,11 @@
+
+
+const HomeBG = () => {
+    return (
+        <div className="home_bg">
+            <p className="text-4xl font-extrabold text-slate-300 text-center">All Your Transportation need is now in a system.</p>
+        </div>
+    );
+};
+
+export default HomeBG;
