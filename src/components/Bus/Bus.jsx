d08a6622a1f1cd9a9d56089b0f6d434a42e7d724
adibmahmud007 <adibmahmud307@gmail.com>
2024-03-29 23:17:08 +0600

    Bus frontend design complete

--- src/components/Bus/Bus.jsx
+++ src/components/Bus/Bus.jsx
@@ -19,9 +19,14 @@ const Bus = () => {
     //     setSelectedStation(stationName);
     //   };
 
+    const handleSelectPerson = e => {
+        const person = e.target.value;
+        console.log(person, 'No of persons from radio');
+    }
+
     return (
         <div className="bg-zinc-900 bus_bg">
-            <div className="flex flex-col min-h-screen">
+            <div className="flex flex-col h-[700px]">
                 <div className="">
                     <Header></Header>
                     <div className="text-white">
@@ -29,53 +34,88 @@ const Bus = () => {
                         <p className="pl-5 md:pl-32 pt-5 md:text-4xl text-xl font-semibold font-sans">Ticket / Fare</p>
                         <p className="p-5 md:text-lg text-sm md:pl-32 pt-5">Select Origin and destination to travel in Bus</p>
                     </div>
-                    <section className="flex gap-20">
-                    <div className="w-[220px] p-5 bg-white rounded-lg translate-y-16 translate-x-32">
-                        <div className="flex gap-10 items-center">
-                            <div>
-                                <div className="w-10"><img src={origin} alt="" /></div>
+                    <section className="flex gap-10">
+                        <div className="w-[220px] p-5 hover:opacity-100 transition-colors opacity-70  bg-white rounded-lg translate-y-16 translate-x-32">
+                            <div className="flex gap-10 items-center">
+                                <div>
+                                    <div className="w-10"><img src={origin} alt="" /></div>
+                                </div>
+                                <div className="">
+                                    <details className="dropdown dropdown-top cursor-pointer dropdown-start">
+                                        <summary className="m-1 text-lg font-bold text-orange-500">Origin</summary>
+                                        <ul className=" shadow menu dropdown-content z-[5] backdrop-blur-lg  rounded-box md:w-52 w-44 text-white">
+                                            {stations.map(station => (
+                                                <li key={station.name} >
+                                                    {station.name}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </details>
+                                </div>
                             </div>
-                            <div className=""> 
-                                <details className="dropdown dropdown-top cursor-pointer dropdown-start">
-                                    <summary className="m-1 text-lg font-bold text-orange-500">Origin</summary>
-                                    <ul className=" shadow menu dropdown-content z-[5] backdrop-blur-lg  rounded-box md:w-52 w-44 text-white">
-                                        {stations.map(station => (
-                                            <li key={station.name} >
-                                            {station.name}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </details>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    <div className="w-10 translate-x-32 translate-y-20 ">
-                        <img className="text-white" src={exchange} alt="" />
-                    </div>
-                    <div className="w-[250px] p-5 bg-white rounded-lg translate-y-16 translate-x-32">
-                        <div className="flex gap-10 items-center">
-                            <div>
-                                <div className="w-10"><img src={destination} alt="" /></div>
-                            </div>
-                            <div className=""> 
-                                <details className="dropdown dropdown-top cursor-pointer dropdown-start">
-                                    <summary className="m-1 text-lg font-bold text-orange-500">Destination</summary>
-                                    <ul className=" shadow menu dropdown-content z-[5] backdrop-blur-lg  rounded-box md:w-52 w-44 text-white">
-                                        {stations.map(station => (
-                                            <li key={station.name} >
-                                            {station.name}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </details>
-                            </div>
-                        </div>
 
-                    </div>
+                        </div>
+                        <div className="w-10 translate-x-32 translate-y-20 ">
+                            <img className="text-white" src={exchange} alt="" />
+                        </div>
+                        <div className="w-[250px] p-5 opacity-70 hover:opacity-100 bg-white rounded-lg translate-y-16 translate-x-32">
+                            <div className="flex gap-10 items-center">
+                                <div>
+                                    <div className="w-10"><img src={destination} alt="" /></div>
+                                </div>
+                                <div className="">
+                                    <details className="dropdown dropdown-top cursor-pointer dropdown-start">
+                                        <summary className="m-1 text-lg font-bold text-orange-500">Destination</summary>
+                                        <ul className=" shadow menu dropdown-content z-[5] backdrop-blur-lg  rounded-box md:w-52 w-44 text-white">
+                                            {stations.map(station => (
+                                                <li key={station.name} >
+                                                    {station.name}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </details>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="w-10 translate-x-32 translate-y-20 ">
+                            <img className="text-white" src={exchange} alt="" />
+                        </div>
+                        <div className="w-[250px] p-5 hover:opacity-100 transition-colors opacity-70  bg-white rounded-lg translate-y-16 translate-x-32">
+                            <div className="flex gap-10 items-center">
+                                <div>
+                                    <div className="w-10"><img src={origin} alt="" /></div>
+                                </div>
+                                <div className="">
+                                    <details className="dropdown dropdown-top cursor-pointer dropdown-start">
+                                        <summary className="m-1 text-lg font-bold text-orange-500">Select Bus</summary>
+                                        <ul className=" shadow menu dropdown-content z-[5] backdrop-blur-lg  rounded-box md:w-52 w-44 text-white">
+                                            {stations.map(station => (
+                                                <li key={station.name} >
+                                                    {station.name}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </details>
+                                </div>
+                            </div>
+
+                        </div>
                     </section>
                     <div>
-
+                        <div className="pl-5 md:pl-32 pt-8 text-white translate-y-10">
+                            <p className="text-sm font-bold md:py-2">Persons:</p>
+                            <div className="pt-3">
+                                <label className="radio" htmlFor="1"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="1" value='1' />1</label>
+                                <label className="pl-6 radio" htmlFor="2"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="2" value='2' />2</label>
+                                <label className="pl-6 radio" htmlFor="3"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="3" value='3' />3</label>
+                                <label className="pl-6 radio" htmlFor="4"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="4" value='4' />4</label>
+                                <label className="pl-6 radio" htmlFor="5"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="5" value='5' />5</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="  pt-5 translate-y-9 md:pt-6 text-white  text-center">
+                        <button className="Buy-button">Buy</button>
                     </div>
                 </div>
             </div>
