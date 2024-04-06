8a15be8aba33b3f89358367e27db49ab0f45fa62
adibmahmud007 <adibmahmud307@gmail.com>
2024-04-07 01:32:32 +0600

    Launch interface created

--- src/components/Launch/Launch.jsx
+++ src/components/Launch/Launch.jsx
@@ -1,26 +1,64 @@
+/* eslint-disable no-const-assign */
 import "../../App.css"
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import doublegreen from "../../assets/dourbe-arrow-green.png"
+// import adult from "../../assets/adult.png"
+import { FaRegUser } from "react-icons/fa";
+import { useState } from "react";
 const Launch = () => {
+
+    const [adcount, setCount] = useState(0);
+    const [chcount, setchCount] = useState(0);
+
+    const chinc = () => { setchCount(chcount => chcount + 1) }
+    const chdec = () => { setchCount(chcount => chcount - 1) }
+
+    // eslint-disable-next-line no-const-assign
+    const inc = () => { setCount(adcount => adcount + 1) }
+    const dec = () => { setCount(adcount => adcount - 1) }
+
+
     return (
         <div className="launch_bg">
             <div className="flex flex-col min-h-screen">
                 <Header></Header>
-                <section className="w-[650px] h-[350px] bg-black opacity-75 translate-x-64 translate-y-14 rounded-md">
+                <section className="w-[650px] h-[400px] bg-black opacity-75 translate-x-64 translate-y-14 rounded-md">
                     <div className="text-white px-5 py-3">
-                        <h1 className="text-4xl font-bold pb-2 font-serif">Get Your Trip</h1>
+                        <h1 className="text-4xl font-bold pb-2 font-serif">Get Your Trip By Launch</h1>
                         <p className="text-sm">Set your origin and destination to travel</p>
                     </div>
                     <section className="px-5 py-3 text-white">
-                        <p>Select destination</p>
-                        <div className="flex gap-5">
-                            <input className="w-[250px] rounded-sm px-2 py-1 text-black border-none" placeholder="Enter Your Origin here" type="text" />
+                        <p>Select Destination</p>
+                        <div className="flex pt-3 gap-5">
+                            <div>
+                                {/* <p>Enter the type of cabin</p> */}
+                                <details className="dropdown cursor-pointer">
+                                    <summary className=" bg-white w-[250px] text-black p-2 rounded-sm">Enter Origin</summary>
+                                    <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                        <li><a>Single Cabin</a></li>
+                                        <li><a>Double Cabin</a></li>
+                                        <li><a>Semi-Double Cabin</a></li>
+                                        <li><a>VIP Cabin</a></li>
+                                    </ul>
+                                </details>
+                            </div>
                             <img className="w-10" src={doublegreen} alt="" />
-                            <input className="w-[250px] rounded-sm p-2 text-black border-none" placeholder="Enter Your Destination here" type="text" />
+                            <div>
+                                {/* <p>Enter the type of cabin</p> */}
+                                <details className="dropdown cursor-pointer">
+                                    <summary className=" bg-white w-[250px] text-black p-2 rounded-sm">Enter Destination</summary>
+                                    <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                        <li><a>Single Cabin</a></li>
+                                        <li><a>Double Cabin</a></li>
+                                        <li><a>Semi-Double Cabin</a></li>
+                                        <li><a>VIP Cabin</a></li>
+                                    </ul>
+                                </details>
+                            </div>
                         </div>
                     </section>
-                    <section className="text-white px-5 py-3 flex gap-20">
+                    <section className="text-white px-5 py-1 flex gap-20">
                         <div>
                             <p>Enter the type of cabin</p>
                             <details className="dropdown cursor-pointer">
@@ -34,7 +72,7 @@ const Launch = () => {
                             </details>
                         </div>
                         <div>
-                            <p>Enter the type of cabin</p>
+                            <p>Select Launch Company</p>
                             <details className="dropdown cursor-pointer">
                                 <summary className=" bg-white w-[250px] text-black p-2 rounded-sm">Select Launch </summary>
                                 <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
@@ -46,8 +84,44 @@ const Launch = () => {
                             </details>
                         </div>
                     </section>
-                    
+                    <section className="flex gap-12">
+                        <div className="pt-4">
+
+                            <div className="flex gap-4 items-center pl-3">
+                                <h1><FaRegUser className="h-10 w-16 text-white" /></h1>
+                                <div className="text-white ">
+                                    <h1 className="text-lg font-semibold">Adults</h1>
+                                    <p className="text-xs">Age 6 and over</p>
+                                </div>
+                                <div className="text-black px-4 py-1 flex gap-3 bg-white items-center rounded-xl">
+                                    <button className="text-xl" onClick={inc}>+</button>
+                                    <div>{adcount}</div>
+                                    <button className="text-2xl" onClick={dec}>-</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pt-4">
+
+                            <div className="flex gap-4 items-center pl-3">
+                                <h1><FaRegUser className="h-7 w-10 text-white" /></h1>
+                                <div className="text-white ">
+                                    <h1 className="text-lg font-semibold">Children</h1>
+                                    <p className="text-xs">Age 2 to 5 years</p>
+                                </div>
+                                <div className="text-black px-4 py-1 flex gap-3 bg-white items-center rounded-xl">
+                                    <button className="text-xl" onClick={chinc}>+</button>
+                                    <div>{chcount}</div>
+                                    <button className="text-2xl" onClick={chdec}>-</button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="pl-6 pt-4">
+                        <button className="bg-lime-500 rounded-2xl px-9 text-lg font-semibold py-2">Buy Ticket</button>
+                    </section>
+
                 </section>
+
             </div>
 
             <Footer></Footer>
