e6c68faf1d2b437c58ef8ed1a8a32690561fe0d9
adibmahmud007 <adibmahmud307@gmail.com>
2024-04-13 21:31:41 +0600

    Plane UI updated

--- src/components/Plane/Plane.jsx
+++ src/components/Plane/Plane.jsx
@@ -23,7 +23,7 @@ const Launch = () => {
         <div className="plane_bg ">
             <div className="flex flex-col px-1 py-2 min-h-screen">
                 <Header></Header>
-                <section className="md:w-[650px] md:h-[400px] mx-auto p-2 bg-white opacity-75  md:translate-y-14 rounded-md">
+                <section className="md:w-[670px] md:h-[420px] mx-auto p-2 bg-white opacity-75  md:translate-y-14 rounded-md">
                     <div className="text-black px-5 py-3">
                         <h1 className="md:text-4xl text-3xl font-bold pb-2 font-serif">Get Your Trip By Launch</h1>
                         <p className="text-sm">Set your origin and destination to travel</p>
@@ -36,7 +36,7 @@ const Launch = () => {
                                 {/* <p>Enter the type of cabin</p> */}
                                 <details className="dropdown cursor-pointer pb-2 md:pb-0">
                                     
-                                    <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">Enter Origin</summary>
+                                    <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">City or Airport</summary>
                                     <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                                         <li><a>Single Cabin</a></li>
                                         <li><a>Double Cabin</a></li>
@@ -45,11 +45,13 @@ const Launch = () => {
                                     </ul>
                                 </details>
                             </div>
-                            <img className="md:translate-x-0 translate-x-64 w-10" src={doublegreen} alt="" />
-                            <div>
+                            <img className="md:translate-x-0 translate-x-64 w-10 h-14 pt-3" src={doublegreen} alt="" />
+                            <div className="border border-black p-2">
+                            <p className="text-sm pl-2 font-semibold">Flying To</p>
                                 {/* <p>Enter the type of cabin</p> */}
-                                <details className="dropdown cursor-pointer pt-2 md:pt-0">
-                                    <summary className=" bg-white w-[300px]  md:w-[250px] text-black p-2 rounded-sm">Enter Destination</summary>
+                                <details className="dropdown cursor-pointer pb-2 md:pb-0">
+                                    
+                                    <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">City or Airport</summary>
                                     <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                                         <li><a>Single Cabin</a></li>
                                         <li><a>Double Cabin</a></li>
@@ -61,7 +63,7 @@ const Launch = () => {
                         </div>
                     </section>
                     <section className="text-white px-5 py-1 md:flex gap-20">
-                        <div className="md:pt-0 pt-3">
+                        <div className="md:pt-0 pt-3 border border-black">
                             <p className="text-sm">Enter the type of cabin</p>
                             <details className="dropdown cursor-pointer pb-5">
                                 <summary className=" bg-white w-[300px]  md:w-[250px] text-black p-2 rounded-sm">Select Cabin type</summary>
@@ -90,13 +92,13 @@ const Launch = () => {
                         <div className="md:pt-0 pt-4">
 
                             <div className="flex gap-4 items-center pl-3">
-                                <h1><FaRegUser className="h-10 w-16 text-white" /></h1>
-                                <div className="text-white ">
+                                <h1><FaRegUser className="h-10 w-16 text-black" /></h1>
+                                <div className="text-black ">
                                     <h1 className="text-lg font-semibold">Adults</h1>
                                     <p className="text-xs">Age 6 and over</p>
                                 </div>
-                                <div className="text-black px-4 py-1 flex gap-3 bg-white items-center rounded-xl">
-                                    <button className="text-xl" onClick={inc}>+</button>
+                                <div className="text-white px-4 py-1 flex gap-3 bg-black items-center rounded-xl">
+                                    <button className="text-xl " onClick={inc}>+</button>
                                     <div>{adcount}</div>
                                     <button className="text-2xl" onClick={dec}>-</button>
                                 </div>
@@ -105,12 +107,12 @@ const Launch = () => {
                         <div className="md:pt-0 pt-4">
 
                             <div className="flex gap-4 items-center pl-8 md:pb-0 pb-2">
-                                <h1><FaRegUser className="h-7 w-10 text-white" /></h1>
-                                <div className="text-white ">
+                                <h1><FaRegUser className="h-7 w-10 text-black" /></h1>
+                                <div className="text-black ">
                                     <h1 className="text-lg font-semibold">Children</h1>
                                     <p className="text-xs">Age 2 to 5 years</p>
                                 </div>
-                                <div className="text-black px-4 py-1 flex gap-3 bg-white items-center rounded-xl">
+                                <div className="text-white px-4 py-1 flex gap-3 bg-black items-center rounded-xl">
                                     <button className="text-xl" onClick={chinc}>+</button>
                                     <div>{chcount}</div>
                                     <button className="text-2xl" onClick={chdec}>-</button>
