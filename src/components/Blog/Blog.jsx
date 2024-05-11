b96c3de0e61e6dedf546f0895a0df4f2dc3ee38b
adibmahmud007 <adibmahmud307@gmail.com>
2024-05-11 16:12:55 +0600

    reset page modified

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import img1 from '../../assets/images/img_1.jpg';
 import img2 from '../../assets/images/img_2.jpg';
--- src/components/Reset/Reset.jsx
+++ src/components/Reset/Reset.jsx
@@ -1,5 +1,5 @@
 
-import { useState,useParams } from "react";
+import { useState, useParams } from "react-router-dom";
 import axios from "axios";
 
 const Reset = () => {
@@ -10,26 +10,31 @@ const Reset = () => {
         const newpass = e.target.value;
         setPassword(newpass);
     }
-    const { username,token } = useParams();
+    const { username, token } = useParams();
 
-    const handleSubmit=()=>{
-        
-            axios.post(`http://localhost:8000/api/v1/password/reset_password/${username}/${token}`,{
-                password
-            },{withCredentials:true}) 
-            .then(res=>{
+    const handleSubmit = () => {
+
+        axios.post(`http://localhost:8000/api/v1/password/reset_password/${username}/${token}`, {
+            password
+        }, { withCredentials: true })
+            .then(res => {
                 console.log(res.data.message);
             })
-        
+
     }
 
     return (
         <div>
-            <section>
-                <label onChange={handlePasswordChange} htmlFor="">
-                    <input className="px-4 py-2 border border-black w-[350px] " type="password" value={password} />
-                </label>
-                <button onClick={handleSubmit}>Submit</button>
+            <section className="bg-zinc-900">
+                <h1 className="text-4xl text-center font-bold text-white">Reset Password</h1>
+                <div className="w-[500px] h-[350px] bg-white rounded-xl pt-5 pl-5 ">
+                    <div>
+                        <label className="pb-2" onChange={handlePasswordChange} htmlFor="">
+                            <input className="px-4 py-2 border border-black w-[350px] " type="password" placeholder="Enter New Password" value={password} />
+                        </label>
+                    </div>
+                    <button className="btn" onClick={handleSubmit}>Submit</button>
+                </div>
             </section>
         </div>
     );
