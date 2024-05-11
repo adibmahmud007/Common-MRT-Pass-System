631e974fa5505dc119b6e6ecb631647f54e6fbda
adibmahmud007 <adibmahmud307@gmail.com>
2024-05-11 16:31:58 +0600

    reset modified

--- src/components/Reset/Reset.jsx
+++ src/components/Reset/Reset.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import toast, { Toaster } from 'react-hot-toast';
 
@@ -30,15 +30,15 @@ const Reset = () => {
             <div><Toaster /></div>
             <section className="bg-zinc-900 h-screen ">
                 <h1 className="text-4xl text-center font-bold text-white pb-4">Reset Password</h1>
-                <div className="w-[450px] h-[150px] bg-white rounded-xl pt-5 pl-5 mx-auto  ">
+                <div className="w-[400px] h-[150px] bg-white rounded-xl pt-5 pl-5 mx-auto  ">
                     <div>
                         <label className="mb-2" onChange={handlePasswordChange} htmlFor="">
                             <input className="px-4 py-2 border rounded-lg  border-black w-[350px] " type="password" placeholder="Enter New Password" value={password} />
                         </label>
                     </div>
-                    <div className="mt-2">
+                    <div className="mt-2 flex justify-between">
                         <button className="btn" onClick={handleSubmit}>Submit</button>
-
+                        <Link to="/login"><button className="btn mr-8">Login</button></Link>
                     </div>
                 </div>
             </section>
