b7d129a0c99d679eaf0e19f7ad539465c9ff8c6e
adibmahmud007 <adibmahmud307@gmail.com>
2024-05-14 20:03:35 +0600

    date picker added

--- src/components/Plane/Plane.jsx
+++ src/components/Plane/Plane.jsx
@@ -5,7 +5,7 @@ import Header from "../Header/Header";
 import doublegreen from "../../assets/dourbe-arrow-green.png"
 // import adult from "../../assets/adult.png"
 // import { FaRegUser } from "react-icons/fa";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import axios from 'axios';
 // import React, { useState } from "react";
@@ -14,8 +14,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 // import DatePicker from "react-date-picker";
 const Plane = () => {
-    const [startDate, setStartDate] = useState(new Date());
-  
+    const [selectedDate, setSelectedDate] = useState(null);
     // <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
 
 
@@ -31,9 +30,9 @@ const Plane = () => {
     const [numOfPassenger, setNumOfPassenger] = useState();
     const [fare, settotalCost] = useState();
     const uniqueAirplanes = [...new Set(airplanes.map(airplane => airplane.name))];
-    const originStation=departureAirport;
-    const destinationStation=arrivalAirport;
-    const transportName=name;
+    const originStation = departureAirport;
+    const destinationStation = arrivalAirport;
+    const transportName = name;
     const transportMedium = stations.length > 0 ? stations[0].transport_medium : undefined;
 
     // const chinc = () => { setchCount(chcount => chcount + 1) }
@@ -47,19 +46,19 @@ const Plane = () => {
         setNumOfPassenger(person);
         console.log(person, 'No of persons from radio');
     }
-    const handleDestinationSelect=(stationName)=>{
+    const handleDestinationSelect = (stationName) => {
         setDestinationStation(stationName);
     }
 
     const handleStationSelect = (stationName) => {
         setOriginStation(stationName);
-      };
+    };
     const handleAirplaneNameSelect = (Name) => {
         setAirplaneName(Name);
-      };
+    };
     const handleTimeSelect = (Time) => {
         setTime(Time);
-      };
+    };
 
     useEffect(() => {
         axios.get("http://localhost:8000/api/v1/stations/airplane")
@@ -82,7 +81,7 @@ const Plane = () => {
                 if (Array.isArray(response.data.data)) {
                     setAirplanes(response.data.data);
                 }
-               
+
                 console.log(response.data.data);
             } catch (error) {
                 console.error('Error fetching airplane:', error);
@@ -99,7 +98,7 @@ const Plane = () => {
                     departureAirport,
                     arrivalAirport
                 }, { withCredentials: true });
-                
+
                 if (Array.isArray(response.data.data)) {
                     setTimes(response.data.data);
                 }
@@ -109,38 +108,38 @@ const Plane = () => {
             }
         };
         fetchTime();
-    }, [name,departureAirport, arrivalAirport]);
+    }, [name, departureAirport, arrivalAirport]);
 
 
-    if(departureAirport!==undefined && arrivalAirport!==undefined && departureTime!==undefined && numOfPassenger!==undefined){
-        console.log(name,departureAirport,arrivalAirport,departureTime,numOfPassenger);
-        axios.post("http://localhost:8000/api/v1/fare/calculate/airplane",{
+    if (departureAirport !== undefined && arrivalAirport !== undefined && departureTime !== undefined && numOfPassenger !== undefined) {
+        console.log(name, departureAirport, arrivalAirport, departureTime, numOfPassenger);
+        axios.post("http://localhost:8000/api/v1/fare/calculate/airplane", {
             name,
             departureAirport,
             arrivalAirport,
             departureTime,
             numOfPassenger
-            
-        },{withCredentials:true})
-        .then(res => {
-            settotalCost(res.data.data)
-            console.log(res.data.data);
-        })
-        .catch(error => {
-            console.error('Error fetching fare:', error);
-        });
-     }
 
-     const handlePay = () => {
+        }, { withCredentials: true })
+            .then(res => {
+                settotalCost(res.data.data)
+                console.log(res.data.data);
+            })
+            .catch(error => {
+                console.error('Error fetching fare:', error);
+            });
+    }
 
-        axios.post("http://localhost:8000/api/v1/fare/pay",{transportName,originStation,destinationStation,numOfPassenger,transportMedium,fare}, { withCredentials: true })
+    const handlePay = () => {
+
+        axios.post("http://localhost:8000/api/v1/fare/pay", { transportName, originStation, destinationStation, numOfPassenger, transportMedium, fare }, { withCredentials: true })
             .then(response => {
-                 toast.success('Pay successful');
+                toast.success('Pay successful');
                 console.log(response.data, 'from axios pay'); // This will log the response data
             })
 
             .catch(error => {
-                 toast.error(error.response.data.error.explanation)
+                toast.error(error.response.data.error.explanation)
                 console.error('Error fetching data:', error);
             });
     }
@@ -160,13 +159,13 @@ const Plane = () => {
 
                         <div className="md:flex pt-3 gap-5">
                             <div className="border border-black p-2">
-                            <p className="text-sm pl-2 font-semibold">Flying From: {departureAirport}</p>
+                                <p className="text-sm pl-2 font-semibold">Flying From: {departureAirport}</p>
                                 {/* <p>Enter the type of cabin</p> */}
                                 <details className="dropdown cursor-pointer pb-2 md:pb-0">
 
                                     <summary className=" bg-white w-[350px] md:w-[250px] text-black p-2 rounded-sm">Select Airport</summary>
                                     <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                                    {stations.map(station => (
+                                        {stations.map(station => (
                                             <li key={station.name} onClick={() => handleStationSelect(station.name)}>
                                                 {station.name}
                                             </li>
@@ -176,13 +175,13 @@ const Plane = () => {
                             </div>
                             <img className="md:translate-x-0 translate-x-64 w-10 h-14 pt-3" src={doublegreen} alt="" />
                             <div className="border border-black p-2">
-                            <p className="text-sm pl-2 font-semibold">Flying From: {arrivalAirport}</p>
+                                <p className="text-sm pl-2 font-semibold">Flying From: {arrivalAirport}</p>
                                 {/* <p>Enter the type of cabin</p> */}
                                 <details className="dropdown cursor-pointer pb-2 md:pb-0">
 
                                     <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">Select Airport</summary>
                                     <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-62">
-                                    {stations.map(station => (
+                                        {stations.map(station => (
                                             <li key={station.name} onClick={() => handleDestinationSelect(station.name)}>
                                                 {station.name}
                                             </li>
@@ -197,23 +196,23 @@ const Plane = () => {
                             <h3 className="text-black text-sm font-semibold">Airplane:{name}</h3>
                             <details className="dropdown cursor-pointer pb-2 md:pb-0">
 
-                            <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">Select airplane</summary>
-                            <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                            {uniqueAirplanes.map(Name => (
+                                <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">Select airplane</summary>
+                                <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                    {uniqueAirplanes.map(Name => (
                                         <li key={Name} onClick={() => handleAirplaneNameSelect(Name)}>
                                             {Name}
                                         </li>
                                     ))}
-                            </ul>
-                        </details>
-                            
+                                </ul>
+                            </details>
+
                         </div>
                         <div className="md:pt-0 pt-3 pb-3 px-3 mb-2 text-left border border-black">
                             <h3 className="text-black text-sm font-semibold">Departing:{departureTime}</h3>
                             <details className="dropdown cursor-pointer pb-2 md:pb-0">
 
-                                    <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">Select time</summary>
-                                    {/* <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                <summary className=" bg-white w-[300px] md:w-[250px] text-black p-2 rounded-sm">Select time</summary>
+                                {/* <ul className="p-2 text-black shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                                     {departureTimes.map(time => {
                                             const departureDateTime = new Date(time);
                                             const formattedDateTime = `${departureDateTime.getFullYear()}-${String(departureDateTime.getMonth() + 1).padStart(2, '0')}-${String(departureDateTime.getDate()).padStart(2, '0')} ${String(departureDateTime.getHours()).padStart(2, '0')}:${String(departureDateTime.getMinutes()).padStart(2, '0')}:${String(departureDateTime.getSeconds()).padStart(2, '0')}`;
@@ -224,29 +223,35 @@ const Plane = () => {
                                             );
                                         })}
                                     </ul> */}
-                                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-
-                                </details>
+                                <div>
+                                    <h1>Select a Date</h1>
+                                    <DatePicker
+                                        selected={selectedDate}
+                                        onChange={(date) => setSelectedDate(date)}
+                                        dateFormat="MM/dd/yyyy"
+                                    />
+                                </div>
+                            </details>
                         </div>
                     </section>
                     <section className="md:flex gap-12">
-                    <div className="md:pt-0 pt-4">
+                        <div className="md:pt-0 pt-4">
 
-                        <div className="text-black  pl-5">
-                            <p className="text-sm font-bold md:py-2">Persons:</p>
-                            <div className="pt-3">
-                                <label className="radio border-none" htmlFor="1"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="1" value='1' />1</label>
-                                <label className="pl-6 border-none radio" htmlFor="2"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="2" value='2' />2</label>
-                                <label className="pl-6 border-none radio" htmlFor="3"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="3" value='3' />3</label>
-                                <label className="pl-6 border-none radio" htmlFor="4"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="4" value='4' />4</label>
-                                <label className="pl-6 border-none radio" htmlFor="5"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="5" value='5' />5</label>
+                            <div className="text-black  pl-5">
+                                <p className="text-sm font-bold md:py-2">Persons:</p>
+                                <div className="pt-3">
+                                    <label className="radio border-none" htmlFor="1"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="1" value='1' />1</label>
+                                    <label className="pl-6 border-none radio" htmlFor="2"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="2" value='2' />2</label>
+                                    <label className="pl-6 border-none radio" htmlFor="3"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="3" value='3' />3</label>
+                                    <label className="pl-6 border-none radio" htmlFor="4"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="4" value='4' />4</label>
+                                    <label className="pl-6 border-none radio" htmlFor="5"><input onChange={handleSelectPerson} type="radio" name="radio-1" id="5" value='5' />5</label>
+                                </div>
+                            </div>
+                            <div className="pl-4 pt-2">
+                                <p>Price:{fare}</p>
                             </div>
                         </div>
-                        <div className="pl-4 pt-2">
-                            <p>Price:{fare}</p>
-                        </div>
-                        </div>
-                    
+
                     </section>
                     <section className="md:pl-6 pt-4 md:pb-0 pb-2 md:text-left text-center">
                         <button onClick={handlePay} className="bg-lime-500 rounded-2xl px-9 text-lg font-semibold py-2">Buy Ticket</button>
